99e852e10441b17bd397fa16e39841cbc5300b70
Dino Srdoč <dino.srdoc@bornfight.com>
2019-12-01 20:32:58 +0100

    solve part 2

--- Days/Day1.fs
+++ Days/Day1.fs
@@ -2,7 +2,20 @@ module Day1
 
 let input = Helpers.getInput 1
 
-let fuelRequirement mass = mass / 3 - 2
+let rec fuelRequirement mass = 
+    let initialRequirement = mass / 3 - 2
+    if initialRequirement > 0 then 
+        let additionalFuel = fuelRequirement initialRequirement
+        if additionalFuel > 0 then
+            initialRequirement + additionalFuel
+        else
+            initialRequirement
+    else
+        initialRequirement
 
 let solve = 
-    input |> Seq.sumBy (int >> fuelRequirement)
+    // input |> Seq.sumBy (int >> fuelRequirement)
+    input
+    |> Seq.map int
+    |> Seq.map fuelRequirement
+    |> Seq.sum
